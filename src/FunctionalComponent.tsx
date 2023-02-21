6d4a2cd7599707495eb646818b7ff4131ec9ef78
Piroska Werner <piroska.werner@gmail.com>
2023-02-21 12:32:55 -0600

    feat: Use themecontext in functional component

--- src/FunctionalComponent.tsx
+++ src/FunctionalComponent.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import  styled from '@emotion/styled'
 // import useContext and the context to be used
 import { useContext } from 'react'
 import { ThemeContext } from './ThemeContext'
@@ -7,12 +7,24 @@ export default function FunctionalComponent() {
 
   // set up variable that will use the context
   const darkTheme = useContext(ThemeContext) 
-  
+
+  // STYLED COMPONENTS
+  const Div = styled.div({
+    margin: '20px',
+    padding: '20px',
+    // set colors based on darkThem variable
+    backgroundColor: darkTheme ? '#000' : 'lightgray',
+    color: darkTheme ? 'white' : 'black',
+  })
+
+
+
   return (
     <div>
       <h3>Function Component</h3>
       <button>Toggle Theme</button>
-      <div>I am a div with a dark and a light theme.</div>
+      <Div>I am a div with a {darkTheme ? 'DARK' : 'LIGHT'} THEME.</Div>
     </div>
   )
 }
+
--- src/ThemeContext.tsx
+++ src/ThemeContext.tsx
@@ -1,13 +1,16 @@
 import { useState, createContext } from 'react'
 
-export const ThemeContext = createContext(false)
+export const ThemeContext = createContext(true)
 
 
 const ThemeProvider = ({children}: {children: React.ReactNode}) => {
+
+  // create state 
   const [ darkTheme, setDarkTheme ] = useState(false)
 
   return (
-    <ThemeContext.Provider value={false}>
+    // pass the current value of the state variable darkTheme as context value
+    <ThemeContext.Provider value={darkTheme}>
       {children}
     </ThemeContext.Provider>
 
